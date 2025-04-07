826d8cd9a55778d8389ab50a1c05f55e1247eaf0
dhruv0154 <dhruvranger97@gmail.com>
2025-04-07 20:53:58 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,30 @@
+// Your code here...
+void caesarCipher(char a[100], int shift, char r[100])
+{
+    for(int i = 0; a[i] != '\0'; i++)
+    {
+        int n = a[i] + shift;
+        if(isupper(a[i]))
+        {
+            if(n > 90)
+            {
+                a[i] = 65 + (90 - n);
+            }
+            else
+            {
+                a[i] = n;
+            }
+        }
+        else if(islower(a[i]))
+        {
+            if(n > 122)
+            {
+                a[i] = 97 + (122 - n);
+            }
+            else
+            {
+                a[i] = n;
+            }
+        }
+    }
+}
