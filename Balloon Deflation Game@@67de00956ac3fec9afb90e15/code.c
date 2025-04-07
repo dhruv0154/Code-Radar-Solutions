5a5aca4049d478c97bb9839931eae1d42ec19eba
dhruv0154 <dhruvranger97@gmail.com>
2025-04-07 18:56:19 +0530

    Solution to Balloon Deflation Game

---
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -0,0 +1,32 @@
+// Your code here...
+
+void deflateBallons(int *air, int n)
+{
+    int n = 0;
+    int smallestNumber = 99999;
+    for(int i = 0; i < n; i++)
+    {
+        if(air[i] <= 0) n++;
+    }
+    printf("%d", n);
+
+    for(int i = 0; i < n; i++)
+    {
+        n = 0;
+        for(int k = 0; k < n; k++)
+        {
+            if(arr[i] < smallestNumber) smallestNumber = arr[i];
+        }
+        for(int j = 0; j < n; j++)
+        {
+            arr[i] -= smallestNumber;
+        }
+
+        for(int s = 0; s < n; s++)
+        {
+            if(arr[s] <= 0) n++;
+        }
+
+        printf("%d", n);
+    }
+}
