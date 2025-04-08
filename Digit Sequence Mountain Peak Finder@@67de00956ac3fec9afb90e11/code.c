796c61e141a2d5739db169a33acd5e406259fd4b
dhruv0154 <dhruvranger97@gmail.com>
2025-04-08 17:20:01 +0530

    Solution to Digit Sequence Mountain Peak Finder

---
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -0,0 +1,45 @@
+// Your code here...
+
+
+int mountainPeak(int n)
+{
+    int greatest = 0;
+    bool peak = false;
+    int index = 0;
+
+    char s[50];
+
+    int size = sprintf(s, "%d", n);
+    for(int i = 0; i < size; i++)
+    {
+        if(atoi(s[i]) > greatest)
+        {
+            greatest = atoi(s[i]);
+            index = i;
+        }
+    }
+
+    for(int j = i; j >= 0; j++)
+    {
+        if(j == 0) break;
+        if(atoi(s[j]) < atoi[s[j - 1]]) 
+        {
+            peak = false;
+            greatest = -1;
+        }
+    }
+
+    if(!peak) return greatest;
+
+    for(int j = 0; j < size; j++)
+    {
+        if(j == size - 1) return;
+        if(atoi(s[j]) < atoi(s[j + 1]))
+        {
+            peak = false;
+            greatest = -1;
+        }
+    }
+
+    return greatest;
+}
