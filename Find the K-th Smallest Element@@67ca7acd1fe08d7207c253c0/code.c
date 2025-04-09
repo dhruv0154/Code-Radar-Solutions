cdc8a6837e68bff564c70c7526b201ca2f2a5cab
dhruv0154 <dhruvranger97@gmail.com>
2025-04-09 16:47:34 +0530

    Solution to Find the K-th Smallest Element

---
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include <stdlib.h>
+
+int cmpr(const void* a, const void* b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+int kthSmallest(int arr[], int n. int k)
+{
+    qsort(arr, n, sizeof(int), cmpr);
+
+    return arr[k - 1];
+}
