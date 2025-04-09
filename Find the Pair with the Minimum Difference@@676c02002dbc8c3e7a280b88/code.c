137611471efee919305ac8d6792943cd44a90420
dhruv0154 <dhruvranger97@gmail.com>
2025-04-09 17:07:27 +0530

    Solution to Find the Pair with the Minimum Difference

---
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -0,0 +1,49 @@
+// Your code here...
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpr(const void* a, const void* b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+int main()
+{
+    int arr[100];
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    qsort(arr, n, sizeof(int), cmpr);
+    int smallest = 999999;
+    int first = 0;
+    int second = 0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        int curr = arr[i] - arr[i - 1];
+
+        if(curr < smallest)
+        {
+            smallest = curr;
+            if(arr[i] > arr[i - 1])
+            {
+                first = arr[i - 1];
+                second = arr[i];
+            }
+            else
+            {
+                first = arr[i];
+                second = arr[i - 1];
+            }
+        }
+    }
+
+    printf("%d %d", first, second);
+    
+}
